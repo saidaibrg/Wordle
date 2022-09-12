8b8e2ccf3ac90cb0dc595435eaecb38822d86141
saidaibrg <ibrag.saida@gmail.com>
2022-09-12 14:01:50 -0400

    Fix the constraint issue

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.wordle
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,30 +13,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Initializes the layout and loads the widgets into the UI
+
         val guessEditText = findViewById<EditText>(R.id.guess)
         val button = findViewById<Button>(R.id.button)
-        var strValue = guessEditText.text.toString()
         val guessTextView=findViewById<TextView>(R.id.textView)
-        button.setOnClickListener(
-            guessTextView.text=strValue
-        )
-
+        val textView1=findViewById<TextView>(R.id.textView4)
+        button.setOnClickListener(View.OnClickListener {
+            var strValue = guessEditText.text.toString()
+            guessTextView.setText(strValue)
+            textView1.setText(strValue)
+        })
+        // Textview from edittext is sent to the main textview
 
     }
 
-    private fun checkGuess(guess: String) : String {
-        var result = ""
-        for (i in 0..3) {
-            if (guess[i] == wordToGuess[i]) {
-                result += "O"
-            }
-            else if (guess[i] in wordToGuess) {
-                result += "+"
-            }
-            else {
-                result += "X"
-            }
-        }
-        return result
-    }
+//    private fun checkGuess(guess: String) : String {
+//        var result = ""
+//        for (i in 0..3) {
+//            if (guess[i] == wordToGuess[i]) {
+//                result += "O"
+//            }
+//            else if (guess[i] in wordToGuess) {
+//                result += "+"
+//            }
+//            else {
+//                result += "X"
+//            }
+//        }
+//        return result
+//    }
 }
